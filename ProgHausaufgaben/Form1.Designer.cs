86b75545f460f04d5225fbcab5d0ee0b42ebe6e6
antarabruzzi <aa@objektkultur.de>
2017-12-01 16:19:54 +0100

    Initial Check-In

--- ProgHausaufgaben/Form1.Designer.cs
+++ ProgHausaufgaben/Form1.Designer.cs
@@ -28,20 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonDatenbankOeffnen = new System.Windows.Forms.Button();
+            this.buttonUebersicht = new System.Windows.Forms.Button();
+            this.buttonAlleFaecher = new System.Windows.Forms.Button();
+            this.buttonAlleFachnamen = new System.Windows.Forms.Button();
+            this.buttonAlleSchulhalbjahre = new System.Windows.Forms.Button();
+            this.buttonAlleSchueler = new System.Windows.Forms.Button();
+            this.buttonZeugnis = new System.Windows.Forms.Button();
+            this.buttonEntwicklung = new System.Windows.Forms.Button();
+            this.buttonEntwicklungSchueler = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonDatenbankOeffnen
+            // 
+            this.buttonDatenbankOeffnen.Location = new System.Drawing.Point(754, 55);
+            this.buttonDatenbankOeffnen.Name = "buttonDatenbankOeffnen";
+            this.buttonDatenbankOeffnen.Size = new System.Drawing.Size(173, 23);
+            this.buttonDatenbankOeffnen.TabIndex = 0;
+            this.buttonDatenbankOeffnen.Text = "Datenbank öffnen";
+            this.buttonDatenbankOeffnen.UseVisualStyleBackColor = true;
+            // 
+            // buttonUebersicht
+            // 
+            this.buttonUebersicht.Location = new System.Drawing.Point(754, 93);
+            this.buttonUebersicht.Name = "buttonUebersicht";
+            this.buttonUebersicht.Size = new System.Drawing.Size(173, 23);
+            this.buttonUebersicht.TabIndex = 1;
+            this.buttonUebersicht.Text = "Übersicht";
+            this.buttonUebersicht.UseVisualStyleBackColor = true;
+            // 
+            // buttonAlleFaecher
+            // 
+            this.buttonAlleFaecher.Location = new System.Drawing.Point(754, 133);
+            this.buttonAlleFaecher.Name = "buttonAlleFaecher";
+            this.buttonAlleFaecher.Size = new System.Drawing.Size(173, 23);
+            this.buttonAlleFaecher.TabIndex = 2;
+            this.buttonAlleFaecher.Text = "alle Fächer";
+            this.buttonAlleFaecher.UseVisualStyleBackColor = true;
+            // 
+            // buttonAlleFachnamen
+            // 
+            this.buttonAlleFachnamen.Location = new System.Drawing.Point(754, 174);
+            this.buttonAlleFachnamen.Name = "buttonAlleFachnamen";
+            this.buttonAlleFachnamen.Size = new System.Drawing.Size(173, 23);
+            this.buttonAlleFachnamen.TabIndex = 3;
+            this.buttonAlleFachnamen.Text = "alle Fachnamen";
+            this.buttonAlleFachnamen.UseVisualStyleBackColor = true;
+            // 
+            // buttonAlleSchulhalbjahre
+            // 
+            this.buttonAlleSchulhalbjahre.Location = new System.Drawing.Point(754, 215);
+            this.buttonAlleSchulhalbjahre.Name = "buttonAlleSchulhalbjahre";
+            this.buttonAlleSchulhalbjahre.Size = new System.Drawing.Size(173, 23);
+            this.buttonAlleSchulhalbjahre.TabIndex = 4;
+            this.buttonAlleSchulhalbjahre.Text = "alle Schulhalbjahre";
+            this.buttonAlleSchulhalbjahre.UseVisualStyleBackColor = true;
+            // 
+            // buttonAlleSchueler
+            // 
+            this.buttonAlleSchueler.Location = new System.Drawing.Point(754, 256);
+            this.buttonAlleSchueler.Name = "buttonAlleSchueler";
+            this.buttonAlleSchueler.Size = new System.Drawing.Size(173, 23);
+            this.buttonAlleSchueler.TabIndex = 5;
+            this.buttonAlleSchueler.Text = "alle Schüler";
+            this.buttonAlleSchueler.UseVisualStyleBackColor = true;
+            // 
+            // buttonZeugnis
+            // 
+            this.buttonZeugnis.Location = new System.Drawing.Point(754, 297);
+            this.buttonZeugnis.Name = "buttonZeugnis";
+            this.buttonZeugnis.Size = new System.Drawing.Size(173, 23);
+            this.buttonZeugnis.TabIndex = 6;
+            this.buttonZeugnis.Text = "Zeugnis für einen Schüler";
+            this.buttonZeugnis.UseVisualStyleBackColor = true;
+            // 
+            // buttonEntwicklung
+            // 
+            this.buttonEntwicklung.Location = new System.Drawing.Point(754, 335);
+            this.buttonEntwicklung.Name = "buttonEntwicklung";
+            this.buttonEntwicklung.Size = new System.Drawing.Size(173, 23);
+            this.buttonEntwicklung.TabIndex = 7;
+            this.buttonEntwicklung.Text = "Entwicklung eines Schülers";
+            this.buttonEntwicklung.UseVisualStyleBackColor = true;
+            // 
+            // buttonEntwicklungSchueler
+            // 
+            this.buttonEntwicklungSchueler.Location = new System.Drawing.Point(754, 373);
+            this.buttonEntwicklungSchueler.Name = "buttonEntwicklungSchueler";
+            this.buttonEntwicklungSchueler.Size = new System.Drawing.Size(173, 23);
+            this.buttonEntwicklungSchueler.TabIndex = 8;
+            this.buttonEntwicklungSchueler.Text = "Entwicklung eines Schülers";
+            this.buttonEntwicklungSchueler.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(754, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Durchschnittsnoten aller Fächer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(754, 450);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Ende";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(949, 557);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonEntwicklungSchueler);
+            this.Controls.Add(this.buttonEntwicklung);
+            this.Controls.Add(this.buttonZeugnis);
+            this.Controls.Add(this.buttonAlleSchueler);
+            this.Controls.Add(this.buttonAlleSchulhalbjahre);
+            this.Controls.Add(this.buttonAlleFachnamen);
+            this.Controls.Add(this.buttonAlleFaecher);
+            this.Controls.Add(this.buttonUebersicht);
+            this.Controls.Add(this.buttonDatenbankOeffnen);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Schulverwaltungssystem";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDatenbankOeffnen;
+        private System.Windows.Forms.Button buttonUebersicht;
+        private System.Windows.Forms.Button buttonAlleFaecher;
+        private System.Windows.Forms.Button buttonAlleFachnamen;
+        private System.Windows.Forms.Button buttonAlleSchulhalbjahre;
+        private System.Windows.Forms.Button buttonAlleSchueler;
+        private System.Windows.Forms.Button buttonZeugnis;
+        private System.Windows.Forms.Button buttonEntwicklung;
+        private System.Windows.Forms.Button buttonEntwicklungSchueler;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
